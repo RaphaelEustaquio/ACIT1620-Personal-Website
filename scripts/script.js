8e4a0ee6dd3d133d22c6d8ab0a09c20084a06f99
Raph <raffy211@gmail.com>
2022-11-08 11:36:21 -0800

    created an array of courses and identified those courses

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+let ACIT1620 = {
+        code:"ACIT 1620",
+        name:"Web Fundamental Technologies",
+}
+
+let ACIT1515 = {
+    code: "ACIT 1515",
+    name: "Scripting for IT"
+}
+
+let ACIT1630 = {
+    code: "ACIT1630",
+    name: "Database Systems"
+}
+const courseList = ['ACIT1620', 'ACIT1515', 'ACIT1630']
+
+let input = "";
+do {
+    input = promt("Enter a 4-digit number")
+}
