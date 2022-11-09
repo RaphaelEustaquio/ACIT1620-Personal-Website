74a9dfe42420b6a1ee9e53ab4174cd6bb02442da
Raph <raffy211@gmail.com>
2022-11-08 16:06:53 -0800

    created the for loop for searching through the array

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,21 @@ do {
     input = prompt("Enter a 4-digit number")
 }
 while(isNaN(Number(input)) | input.toString().length !== 4)
+
+let sign = false
+for (let course of courseList)
+{
+    if(course.code.includes(input)){
+
+    
+    console.log(`Yes I am taking the course: ${course.code} - ${course.name}`)
+    sign = true
+    }
+    
+}
+
+if(sign == false)
+{
+    courseList.push({code:input, name:null})
+    console.log("Success")
+}
